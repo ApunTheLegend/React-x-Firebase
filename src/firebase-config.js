57ce6a941843c76031b0afc30654990241550139
Sam <96652770+ApunTheLegend@users.noreply.github.com>
2022-02-03 02:30:14 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -11,12 +11,12 @@ import { getFirestore } from 'firebase/firestore'
 // }
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyAKyckxZD-Ok3TXz2iYHXOpYcShKMcYxjY',
-    authDomain: 'ventech-906a4.firebaseapp.com',
-    projectId: 'ventech-906a4',
-    storageBucket: 'ventech-906a4.appspot.com',
-    messagingSenderId: '296200018086',
-    appId: '1:296200018086:web:354fc81ba08fdbd096bc67'
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
